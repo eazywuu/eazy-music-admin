f375f5fa4007c9562eb7e6df876484963b138817
wyz <2947680284@qq.com>
2022-11-29 19:49:18 +0800

    feat: add quasar and axios request

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,33 @@
 <script setup>
 import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
+import { sayHello } from '../api/hello'
 
 const count = ref(0)
+const message = ref('eazy-music backstage')
+
+const hi = () => {
+  sayHello().then((res) => {
+    console.log(res)
+  })
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <h1>{{ message }}</h1>
+  <div class="button-container">
+    <q-btn color="primary" @click="count++">
+      click {{ count }}
+    </q-btn>
+    <br>
+    <br>
+    <q-btn color="primary" @click="hi">
+      发起请求！
+    </q-btn>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+/* .button-container{
+
+} */
 </style>
